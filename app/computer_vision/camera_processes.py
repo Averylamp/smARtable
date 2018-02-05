91a19de98b5e39998e7f2bfc3ba110bdda5b0e1c
Ethan Weber <ethanweber@dhcp-18-189-121-153.dyn.MIT.EDU>
2018-02-05 03:44:37 -0500

    adding new vision code

---
+++ app/computer_vision/camera_processes.py
@@ -0,0 +1,34 @@
+from vision import *
+import cv2
+
+# this function is used to compute the best point from
+def filter_best_point(camera_objects, THRESH=150, KERNEL=(30,30)):
+    '''
+    Pass in the camera object list to get an averaged point result.
+    '''
+    points = []
+    for camera_object in camera_objects:
+        points.append(camera_object.get_box_of_interest(THRESH=THRESH, KERNEL=KERNEL))
+
+    # get average where the point is not none
+    x_average = 0.0
+    y_average = 0.0
+    num_points = 0.0
+    for point in points:
+        if point is not None:
+            num_points += 1.0
+            x_average += point[0]
+            y_average += point[1]
+    x_average /= point_num
+    y_average /= point_num
+    return (math.floor(x_average), math.floor(y_average))
+
+def get_item_class(camera_objects):
+    # use the first camera frame to get the google vision API output
+    ret, frame = camera_objects[0].grab_image()
+    cv2.imwrite("temp.jpg", frame)
+    result = get_google_analysis("temp.jpg")
+    # result has "items" and "best_guess"
+    best = result["best_guess"]
+    print("Best Guess: {}".format(best))
+    return best
---
+++ app/computer_vision/vision.py
@@ -0,0 +1,104 @@
+import argparse
+import base64
+import json
+import sys
+import functools
+import requests
+
+
+
+def generate_json_data(image_filename, output_filename):
+    """Translates the input file into a json output file.
+
+    Args:
+        input_file: a file object, containing lines of input to convert.
+        output_filename: the name of the file to output the json to.
+    """
+    request_list = []
+
+    DETECTION_TYPES = [
+    'FACE_DETECTION:10',
+    'CROP_HINTS:10',
+    'LOGO_DETECTION:10',
+    'LABEL_DETECTION:10',
+    'TEXT_DETECTION:10',
+    'WEB_DETECTION:10'
+    ]
+    features = "4:10"
+    with open(image_filename, 'rb') as image_file:
+        content_json_obj = {
+            'content': base64.b64encode(image_file.read()).decode('UTF-8')
+        }
+
+    feature_json_obj = []
+    for detectionType in DETECTION_TYPES:
+        feature, max_results = detectionType.split(':', 1)
+        feature_json_obj.append({
+            'type': feature,
+            'maxResults': int(max_results),
+        })
+
+    request_list.append({
+        'features': feature_json_obj,
+        'image': content_json_obj,
+    })
+
+    with open(output_filename, 'w') as output_file:
+        json.dump({'requests': request_list}, output_file)
+
+
+def get_google_analysis(image_filename):
+    # Will give a response in the form of a dictionary with
+    #    ["crop"] = approximate bounding box (x,y,width,height) if not found
+    #    ["items"] = possible names of the item
+    #    ["best_guess"] = Hopefully string readible title for the object
+
+    output_filename = "google_image_data.json"
+
+    generate_json_data(image_filename, output_filename)
+    data = open(output_filename, 'rb').read()
+
+    response = requests.post(url='https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBgalC41vkCLty97Je2bmgd9nXH8GeIyJA', data=data, headers={'Content-Type': 'application/json'})
+    
+    response_json = response.json()["responses"][0]
+    results = {}
+
+    if "cropHintsAnnotation" in response_json:
+        vertices = response_json["cropHintsAnnotation"]["cropHints"][0]["boundingPoly"]["vertices"]
+        print("Raw Cropping Vertices --- {}".format(vertices))
+        x_vars = []
+        y_vars = []
+        for vertex_pair in vertices:
+            x_vars.append(vertex_pair.get("x", 0))
+            y_vars.append(vertex_pair.get("y", 0))
+
+        results["crop"] = (min(x_vars), min(y_vars), max(x_vars) - min(x_vars),max(y_vars) - min(y_vars))
+    else:
+        results["crop"] = (0,0,0,0)
+    best_guess = ""
+    items = set()
+
+    if "labelAnnotations" in response_json:    
+        entities = list(functools.reduce(lambda a, b: a + b ,map(lambda x: x.get("description").lower().split(" "), filter(lambda d: "description" in d, response_json["labelAnnotations"]))))
+        items.update(entities)
+        best_guess = response_json["labelAnnotations"][0]["description"]
+
+    if "webDetection" in response_json:
+        if "webEntities" in response_json["webDetection"]:
+            entities = list(functools.reduce(lambda a, b: a + b ,map(lambda x: x.get("description").lower().split(" "), filter(lambda d: "description" in d, response_json["webDetection"]["webEntities"]))))
+            items.update(entities)
+        if "bestGuessLabels" in response_json["webDetection"]:
+            best_guess = response_json["webDetection"]["bestGuessLabels"][0]["label"]
+
+
+    results["items"] = list(items)
+    results["best_guess"] = best_guess
+    return results
+
+
+result = get_google_analysis("coffee.jpg")
+
+print(result)
+
+
+
